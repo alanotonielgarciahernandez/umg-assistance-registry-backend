d5eaf67a9d440b1343f6744cc534f019899fa2b2
Crimul <crimul.cr@gmail.com>
2026-04-01 22:27:28 -0600

    Add test route to handlers

--- handlers/handlers.py
+++ handlers/handlers.py
@@ -6,8 +6,10 @@ from django.urls import path
 
 # Importar rutas.
 from routes.personas_route import list_personas
+from routes.endpc import test
 
 # Definir rutas.
 urlpatterns = [
     path( 'personas', list_personas, name='list_personas' ),
+    path( 'test', test ),
 ]
