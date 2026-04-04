194da93939ae4ac0a971d1737cabb240b2522a97
Crimul <crimul.cr@gmail.com>
2026-04-03 20:55:54 -0600

    Add CSRF exemption to login route

--- handlers/handlers.py
+++ handlers/handlers.py
@@ -4,15 +4,19 @@
 # Importar módulos de Django.
 from django.http import HttpResponse
 from django.urls import path
+from django.views.decorators.csrf import csrf_exempt  # 👈 Aquí traemos el pase VIP
 
 # Importar rutas.
 from routes.personas_route import list_personas
 from routes.login_route import login
 
-# Definir rutas.
+# rutas
 urlpatterns = [
+    # ruta de personas
     path( 'personas', list_personas, name='list_personas' ),
-    path( 'login', login, name='login' ),
+    
+    # pase vip para probar login
+    path( 'login', csrf_exempt(login), name='login' ),
 
     # Ruta para manejar cualquier otra ruta no definida (404).
     path( '', lambda _: HttpResponse( '<h1>404 Not Found</h1>', status=404 ) ),
